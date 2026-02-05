831d7c2cf18e56f95241c5174d1f7bef242c52c9
kimaldrich24 <kimaldrichordiz@gmail.com>
2026-02-06 05:49:03 +0800

    admin doctor test 2

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ import staffRoutes from "./routes/staffRoutes.js"
 import adminStaffRoutes from "./routes/adminStaffRoutes.js"
 import { startScheduleCleanup } from './utils/scheduleCleanup.js' // ✅ ADD THIS
 import paymentProofRoutes from './routes/paymentProofRoutes.js';
-import scheduleRoutes from "./routes/scheduleRoutes.js"; // your new schedule router
+import AdminSchedule from '../admin/src/pages/Admin/AdminSchedule.jsx'
 // Node Fetch for SMS
 import fetch from 'node-fetch'
 
@@ -90,7 +90,7 @@ app.use("/api/equipment", equipmentRoutes);
 app.use("/api/staff", staffRoutes)
 app.use("/api/admin/staff", adminStaffRoutes)
 app.use('/api/payment-proofs', paymentProofRoutes);
-app.use("/api/admin", scheduleRoutes); // ✅ important: same prefix as front-end expects
+app.use("/api/admin", AdminSchedule); // ✅ important: same prefix as front-end expects
 app.use('/uploads', express.static('uploads'));
 // root endpoint
 app.get('/', (req, res) => {
