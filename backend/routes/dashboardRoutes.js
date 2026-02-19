7881a908f8632ca976d6b79c838b695f51f70d25
Kim Aldrich Ordiz <kimaldrichordiz@gmail.com>
2026-02-20 06:25:20 +0800

    patch

--- backend/routes/dashboardRoutes.js
+++ backend/routes/dashboardRoutes.js
@@ -1,10 +1,13 @@
 // backend/routes/dashboardRoutes.js
 import express from "express";
 import { getDashboardStats, getRecentAppointments } from "../controllers/dashboardController.js";
+import { getCredits } from "../controllers/dashboardController.js";
 
 const router = express.Router();
 
 router.get("/stats", getDashboardStats);
 router.get("/recent-appointments", getRecentAppointments);
+// GET /dashboard/credits
+router.get("/credits", getCredits);
 
 export default router;
