0a026e6adcc5f41bd834f7ac06dc8ecb6ab4c324
Kim Aldrich Ordiz <kimaldrichordiz@gmail.com>
2026-02-20 04:04:54 +0800

    puyaters 2

--- backend/config/multer.js
+++ backend/config/multer.js
@@ -1,5 +1,12 @@
 import multer from 'multer';
 import path from 'path';
+import fs from 'fs';
+
+// Ensure the folder exists
+const uploadDir = 'uploads/payment-proofs';
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
 
 // Storage configuration for payment screenshots
 const paymentStorage = multer.diskStorage({
