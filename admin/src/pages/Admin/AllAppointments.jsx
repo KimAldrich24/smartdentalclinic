87c65f56eebd37a271d9d30c04e79a050f8c4450
Kim Aldrich Ordiz <kimaldrichordiz@gmail.com>
2026-03-11 14:25:21 +0800

    changes

--- admin/src/pages/Admin/AllAppointments.jsx
+++ admin/src/pages/Admin/AllAppointments.jsx
@@ -207,7 +207,8 @@ const AllAppointments = () => {
               <div className="flex-1 text-sm">
                 <p className="text-lg font-semibold">Dr. {appt.doctor?.name}</p>
                 <p className="text-gray-500">{appt.doctor?.speciality}</p>
-                <p className="mt-2"><b>Patient:</b> {appt.user?.name}</p>
+                <p className="mt-2"><b>Patient:</b> {appt.patient?.name}</p>
+                <p><b>Booked By:</b> {appt.bookedBy?.name || "Self"}</p>
                 <p><b>Date:</b> {appt.date} | {appt.time}</p>
                 <p>
                   <b>Status:</b>{" "}
