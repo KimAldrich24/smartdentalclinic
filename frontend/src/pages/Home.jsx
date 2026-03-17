a8d5f0e0a2d2e419b0357edb1775032836e646c4
Kim Aldrich Ordiz <kimaldrichordiz@gmail.com>
2026-03-17 09:33:35 +0800

    home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,23 +1,31 @@
-import React from 'react'
-import Header from '../components/Header'
-import SpecialityMenu from '../components/SpecialityMenu'
-import TopDoctors from '../components/TopDoctors'
-import Banner from '../components/Banner'
+// Home.jsx
+import React from 'react';
+import Header from '../components/Header';
+import SpecialityMenu from '../components/SpecialityMenu';
+import TopDoctors from '../components/TopDoctors';
+import Banner from '../components/Banner';
 import Reviews from '../components/Reviews';
+import AddReview from '../components/AddReview';
 
 const Home = () => {
   return (
     <div>
-        <Header/>
-       {/*  <SpecialityMenu/>  */}
-        <TopDoctors/>
-        {/* ⭐ REVIEWS SECTION */}
-        <div className="my-10 px-4 md:px-10">
-          <Reviews />
-        </div>
-        <Banner/>
-    </div>
-  )
-}
+      <Header />
+      {/* <SpecialityMenu /> */}
+      <TopDoctors />
 
-export default Home
+      {/* ⭐ REVIEW SECTION */}
+      <div className="my-10 px-4 md:px-10 space-y-6">
+        {/* AddReview: form for patients to submit a review */}
+        <AddReview />
+
+        {/* Reviews: show all existing reviews */}
+        <Reviews />
+      </div>
+
+      <Banner />
+    </div>
+  );
+};
+
+export default Home;
