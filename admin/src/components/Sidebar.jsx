8ef73d749d2c7fadba09d5b298bf913c51ecc3be
Kim Aldrich Ordiz <kimaldrichordiz@gmail.com>
2026-03-18 12:40:16 +0800

    patch 3

--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -129,7 +129,7 @@ const Sidebar = () => {
       {/* Admin-Only Links */}
       {userRole === "admin" && (
         <>
-          <NavLink
+          {/* <NavLink
             onClick={handleLinkClick}
             className={({ isActive }) =>
               `flex items-center gap-2 py-2 px-2 md:py-3.5 md:px-9 cursor-pointer ${isActive ? "bg-[#F2F3FF] border-r-4 border-primary" : ""
@@ -139,7 +139,7 @@ const Sidebar = () => {
           >
             <img className="w-5 h-5 md:w-auto md:h-auto" src={assets.add_icon} alt="Add Doctor" />
             <p className="text-sm md:text-base">Add Dentist</p>
-          </NavLink>
+          </NavLink> */}
 
           <NavLink
             onClick={handleLinkClick}
@@ -258,7 +258,7 @@ const Sidebar = () => {
       </NavLink>
       */}
 
-      <NavLink
+      {/* <NavLink
         onClick={handleLinkClick}
         className={({ isActive }) =>
           `flex items-center gap-2 py-2 px-2 md:py-3.5 md:px-9 cursor-pointer ${isActive ? "bg-[#F2F3FF] border-r-4 border-primary" : ""
@@ -268,7 +268,7 @@ const Sidebar = () => {
       >
         <img className="w-5 h-5 md:w-auto md:h-auto" src={assets.pending_icon || assets.add_icon} alt="Staff Management" />
         <p className="text-sm md:text-base">👥 Receptionist</p>
-      </NavLink>
+      </NavLink> */}
 
       <NavLink
         onClick={handleLinkClick}
