f82ebdc107018e9e961a7ecf2ef8afe41832515d
Kim Aldrich Ordiz <kimaldrichordiz@gmail.com>
2026-03-21 01:49:39 +0800

    sidebar remove job application

--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -202,7 +202,7 @@ const Sidebar = () => {
             <p className="text-sm md:text-base">Equipment Maintenance</p>
           </NavLink>
 
-          <NavLink
+          {/* <NavLink
             onClick={handleLinkClick}
             className={({ isActive }) =>
               `flex items-center gap-2 py-2 px-2 md:py-3.5 md:px-9 cursor-pointer ${isActive ? "bg-[#F2F3FF] border-r-4 border-primary" : ""
@@ -212,7 +212,7 @@ const Sidebar = () => {
           >
             <img className="w-5 h-5 md:w-auto md:h-auto" src={assets.job_icon || assets.add_icon} alt="Job Applications" />
             <p className="text-sm md:text-base">Job Applications</p>
-          </NavLink>
+          </NavLink> */}
         </>
       )}
 
